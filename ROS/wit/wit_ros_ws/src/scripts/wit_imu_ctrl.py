43aeca41ee451a0f5d7fadc34b30e7d5846e4089
Vlad Mosoiu <vlad.m@agorarobotics.com>
2024-02-21 16:31:12 +0200

    imu control ported to ros2

--- ROS/wit/wit_ros_ws/src/scripts/wit_imu_ctrl.py
+++ ROS/wit/wit_ros_ws/src/scripts/wit_imu_ctrl.py
@@ -1,12 +1,9 @@
-#!/usr/bin/env python
-# -*- coding:utf-8 -*-
-import rospy
+#!/usr/bin/env python3
+import rclpy
 from std_msgs.msg import String
-from sensor_msgs.msg import MagneticField
 
 def callback(data):
-    rospy.loginfo(data)
-
+    rclpy.logging.get_logger('Set').info(data.data)
 
 def showhelp():
 	print("----------------------------")
@@ -23,43 +20,45 @@ def showhelp():
 	print("----------------------------")
 
 if __name__ == "__main__":
-
-	rospy.init_node("Set")
-	set_pub = rospy.Publisher("wit/cali", String, queue_size=10)
-	print("please input your cmd:")
-	showhelp()
-	while not rospy.is_shutdown():
-		try:
-			string = raw_input("input cmd:")
-			if 'rate' in string:
-				set_pub.publish(string)
-				print("change " + string)
-			elif 'rsw' in string:
-				set_pub.publish('rsw')
-			elif 'baud' in string:
-				set_pub.publish(string)
-				print("change " + string)
-			elif 'b' in string:
-				set_pub.publish("begin")
-				print("begin recording")
-			elif 's' in string:
-				set_pub.publish("stop")
-				print("stop recording")
-			elif 'v' in string:
-				set_pub.publish("version")
-				print("show sensor version")
-			elif 'h' in string:
-				showhelp()
-			elif 'e' in string:
-				print("exti sys\n")
-				exit(0)
-			elif '0' in string:
-				set_pub.publish("exti")
-				print("exti cali mode")
-			elif '9' in string:
-				set_pub.publish("mag")
-				print("enter mag cali mode")
-			else :
-				print("{} cmd no support".format(string))
-		except Exception as e:
-			print(e)
+    rclpy.init()
+    node = rclpy.create_node('Set')
+    set_pub = node.create_publisher(String, 'wit/cali', 10)
+    print("please input your cmd:")
+    showhelp()
+    while rclpy.ok():
+        try:
+            string = input("input cmd:")
+            if 'rate' in string:
+                set_pub.publish(String(data=string))
+                print("change " + string)
+            elif 'rsw' in string:
+                set_pub.publish(String(data='rsw'))
+            elif 'baud' in string:
+                set_pub.publish(String(data=string))
+                print("change " + string)
+            elif 'b' in string:
+                set_pub.publish(String(data="begin"))
+                print("begin recording")
+            elif 's' in string:
+                set_pub.publish(String(data="stop"))
+                print("stop recording")
+            elif 'v' in string:
+                set_pub.publish(String(data="version"))
+                print("show sensor version")
+            elif 'h' in string:
+                showhelp()
+            elif 'e' in string:
+                print("exti sys\n")
+                exit(0)
+            elif '0' in string:
+                set_pub.publish(String(data="exti"))
+                print("exti cali mode")
+            elif '9' in string:
+                set_pub.publish(String(data="mag"))
+                print("enter mag cali mode")
+            else :
+                print("{} cmd no support".format(string))
+        except Exception as e:
+            print(e)
+        rclpy.spin_once(node)
+    rclpy.shutdown()
